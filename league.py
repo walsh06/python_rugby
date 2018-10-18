acfe1f18b916c2e7c56ec47a9ed5e8c2bf3d8573
Liam Walsh <liwals@microsoft.com>
2018-10-18 15:47:07 +0100

    Renamed matchlist->match, updates to leagues

--- league.py
+++ league.py
@@ -1,22 +1,37 @@
-from matchlist import MatchList
+from match import MatchList
 
 class LeagueList():
 
-    def __init__(self):
-        leagues = {}
+    def __init__(self, leagueDict):
+        self._leagues = {}
+        for league in leagueDict:
+            self._leagues[league] = League(league, leagueDict[league]['name'], leagueDict[league]['matches'])
 
-    def getLeague(self, leagueName):
-        pass
+    def getLeagueByName(self, leagueName):
+        for league in self._leagues.items():
+            if league.name == leagueName:
+                return league
+        return None
 
+    def getLeagueById(self, leagueId):
+        if leagueId in self._leagues.keys():
+            return self._leagues[leagueId]
+        else:
+            return None
 
 class League():
 
-    def __init__(self, id, name, matchIdDict, loadMatches=False):
+    def __init__(self, id, name, matchIdDict, initMatches=False):
         self.id = id
         self.name = name
         self.matchIds = matchIdDict
-        if loadMatches:
-            self.matches = MatchList(self.getMatchIds())
+        self._matchesLoaded = False
+        self.teams = []
+        if initMatches:
+            self.loadMatches()
+        else:
+            self.matches = None
+            
 
     def getMatchIds(self, season=None):
         if season:
@@ -26,3 +41,9 @@ class League():
             for season in self.matchIds.keys():
                 matchIdList.extend(self.matchIds[season])
         return matchIdList
+
+    def loadMatches(self):
+        self.matches = MatchList(self.getMatchIds())
+
+    def containsTeam(self, team):
+        return team in self.teams
